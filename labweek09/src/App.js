b99ceb862e9b8678fecfd9b121f0ec43f7bd8f5b
YSaltedFishY <yxwyyhy@hotmail.com>
2024-11-02 17:52:21 -0400

    Week 09 update

---
+++ labweek09/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <p>
+          101434395
+        </p>
+        <p style={{fontSize: '18px'}}>HaoYun Yang</p>
+        <p style={{fontSize: '15px'}}>George Brown College, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
